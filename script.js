bd83807c6a0c7844a74aeeb7d45863e327d6397a
Salavei Dzmitry <salavei.dv@gmail.com>
2020-03-17 11:14:35 +0300

    feat: add handler to filter click select image

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
+"use strict"
+
 window.onload = function(){
     addMenuClickHandler();
     addIphoneScreenSwithHandler();
+    addSlideLeftHandler();
+    addFilterClickHandler();
+
+    addImageBorderHandler();
 }
 
+const addFilterClickHandler = () => {
+    document.querySelector('.filter-list').addEventListener('click', (event) => {
+        if(event.target.classList.contains('filter-list__text')){
+            // console.log(event.target)
+            mixPictures(event.target);
+            removeFilterLinkActive();
+            addFilterLinkActive(event.target);
+        }
+    })
+}
+
+const removeFilterLinkActive = () => {
+    document.querySelectorAll('.filter-list__text').forEach( el => {
+        el.classList.remove('filter-list__item_active');
+    })
+}
+
+const mixPictures = (target) => {
+    if(!target.classList.contains('filter-list__item_active')){
+        let fig = document.querySelectorAll('.pictures')[0].children
+        let arr = Array();
+        for(let i = 0; i< fig.length; i++) { arr.push(fig[i]) }
+        arr.reverse()
+        arr.forEach(x => {
+            document.querySelectorAll('.pictures')[0].removeChild(x)
+        });
+        arr.forEach(x => {
+            document.querySelectorAll('.pictures')[0].appendChild(x)
+        });
+    }
+    
+}
+
+const addFilterLinkActive = (target) => {
+    target.classList.add('filter-list__item_active')
+}
+
+
+// menu click handler
 const addMenuClickHandler = () => {
     document.querySelector('.menu').addEventListener('click', (event) => {
         if(event.target.classList.contains('menu__link')){
@@ -22,6 +67,7 @@ const addMenuLinkActive = (target) => {
     target.classList.add('menu__link_active')
 }
 
+// screen switch
 const addIphoneScreenSwithHandler = () => {
     document.querySelector('.slider__content').addEventListener('click', (event) => {
         if(event.target.classList.contains('iphone__screen')){
@@ -29,10 +75,97 @@ const addIphoneScreenSwithHandler = () => {
         }
     })
 }
+
 const switchScreen = (target) => {
     if(target.classList.contains('iphone__screen_switched-off')){
         target.classList.remove('iphone__screen_switched-off')
     } else {
         target.classList.add('iphone__screen_switched-off')
     }
+}
+
+
+// slider
+const addSlideLeftHandler = () => {
+    document.querySelector('.slider-control__left').addEventListener('click', (event) => {
+        console.log('left');
+        // console.log(event.target);
+        
+        let items = document.querySelectorAll('.slider__item')
+        // items.forEach(element => {
+            
+        // });
+    })
+}
+
+
+// Image Border
+const addImageBorderHandler = () => {
+    document.querySelector('.pictures').addEventListener('click', (event) => {
+        let border = document.createElement('div');
+        border.classList.add('img_border')
+        border.style.border = "solid 5px #F06C64";
+        border.style.width = "220px";
+        border.style.height = "187px";
+        border.style.position = "relative";
+        border.style.top = "-5px";
+        border.style.left = "-5px";
+        
+        if(event.target.tagName == "IMG"){
+
+            if(document.querySelectorAll('.img_border').length == 0){
+                console.log('add border:')
+            }else{
+                console.log('remove border')
+            }
+
+            let con = document.querySelectorAll('.pictures')[0]
+            let arr = Array();
+            
+            for(let i = 0; i< con.length; i++){
+                arr.push(con[i]);
+            }
+
+            for(let i =0; i< con.children.length; i++){
+                
+                if(con.children[i] == event.target){
+                    if(con.children[i].classList.contains('active')){
+                        con.children[i].style.border = "0px";
+                        con.children[i].style.top = "0px";
+                        con.children[i].style.left = "0px";
+                        con.children[i].classList.remove('active');
+                    } else {
+                        con.children[i].classList.add('active')
+                        con.children[i].style.border = "solid 5px #F06C64";
+                        con.children[i].style.position = "relative";
+                        con.children[i].style.top = "-5px";
+                        con.children[i].style.left = "-5px";
+                    }
+                } else {
+                    console.log('not i:' + i);               
+                    con.children[i].style.border = "0px";
+                    con.children[i].style.position = "relative";
+                    con.children[i].style.top = "0px";
+                    con.children[i].style.left = "0px";
+                }
+            }
+
+
+            // console.log(arr.length)
+            // console.log(con.children.length)
+            // for(let i =0; i< con.children.length; i++){
+            //     document.querySelector('.pictures').removeChild(con.children[i])
+            // }
+            // console.log(arr);
+            
+            // arr.forEach(x => {
+            //     document.querySelector('.pictures').appendChild(x)
+            // });
+
+            // document.querySelectorAll('.pictures')[0].replaceChild(border.appendChild(event.target), event.target)
+            // document.querySelector('.pictures').appendChild(border)
+            
+        }
+
+    })
 }
